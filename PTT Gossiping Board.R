383fec40a863bfcd6162c8556e89def9d5efb216
610311101 <laha610311101@gmail.com>
2017-08-29 15:29:40 +0800

    Update PTT Gossiping Board.R

--- PTT Gossiping Board.R
+++ PTT Gossiping Board.R
@@ -8,11 +8,16 @@ x.nodes      <- html_nodes(x.content,".wide:nth-child(2)");
 x.href       <- html_attr(x.nodes,"href");
 ## the newest page index 
 x.index      <- as.numeric(gsub("[^0-9]","",x.href)) + 1;
-x.index
 
 page.url <- paste("https://www.ptt.cc/bbs/Gossiping/index",1:x.index,".html",sep='');
 ##  show the newest 6 page url
-tail(page.url)
+##  tail(page.url)
+# [1] "https://www.ptt.cc/bbs/Gossiping/index25190.html"
+# [2] "https://www.ptt.cc/bbs/Gossiping/index25191.html"
+# [3] "https://www.ptt.cc/bbs/Gossiping/index25192.html"
+# [4] "https://www.ptt.cc/bbs/Gossiping/index25193.html"
+# [5] "https://www.ptt.cc/bbs/Gossiping/index25194.html"
+# [6] "https://www.ptt.cc/bbs/Gossiping/index25195.html"
 
 ##  use the newest 6 page url for example
 page.index <- gsub("https://www.ptt.cc/bbs/Gossiping/||.html","",tail(page.url));
@@ -25,8 +30,14 @@ text.url <- lapply(tail(page.url),function(x){
   return(x.url)
 })
 names(text.url) <- page.index;
-##  show each text url in the newest 6 page url.
-str(text.url,vec.len = 1, nchar.max = 6);
+# str(text.url,vec.len = 1);
+# List of 6
+# $ index25190: chr [1:19] "https://www.ptt.cc/bbs/Gossiping/ ...
+# $ index25191: chr [1:20] "https://www.ptt.cc/bbs/Gossiping/ ...
+# $ index25192: chr [1:19] "https://www.ptt.cc/bbs/Gossiping/ ...
+# $ index25193: chr [1:20] "https://www.ptt.cc/bbs/Gossiping/ ...
+# $ index25194: chr [1:20] "https://www.ptt.cc/bbs/Gossiping/ ...
+# $ index25195: chr [1:11] "https://www.ptt.cc/bbs/Gossiping/ ...
 
 text.list <- lapply(text.url,function(x1){
   x2.index <- gsub("https://www.ptt.cc/bbs/Gossiping/||.html","",x1)
@@ -40,5 +51,4 @@ text.list <- lapply(text.url,function(x1){
   names(x2.text) <- x2.index;
   return(x2.text)
 })
-##  [[2]][[7]] is the 7th text of the 2nd page index. 
-pander(text.list[[2]][7]);
+# pander(text.list);
